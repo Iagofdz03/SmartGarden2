064c1fa4719e999a0af9b5a7a077fe9fb443e4e6
Iagofdz03 <iagofzdz@gmail.com>
2025-11-06 13:52:05 +0100

    Update SettingsActivity.kt

--- V3/SettingsActivity.kt
+++ V3/SettingsActivity.kt
@@ -1,15 +1,17 @@
 package com.example.smartgarden2
 
 import android.os.Bundle
-import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class SettingsActivity : BaseMenuActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
 
-        val texto = findViewById<TextView>(R.id.textSettings)
-        texto.text = "⚙️ Ajustes de SmartGarden"
+        // Mensaje opcional para comprobar que la pantalla funciona
+        Toast.makeText(this, "Ajustes abiertos", Toast.LENGTH_SHORT).show()
     }
 
     override fun onResume() {
