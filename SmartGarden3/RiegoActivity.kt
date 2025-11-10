7faf3d82a4ba4636233614ad9505bd54058440b2
Iagofdz03 <iagofzdz@gmail.com>
2025-11-10 14:06:32 +0100

    Create RiegoActivity.kt

---
+++ SmartGarden3/RiegoActivity.kt
@@ -0,0 +1,32 @@
+package com.example.smartgraden3
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.snackbar.Snackbar
+
+class RiegoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_riego)
+        title = "Riego"
+
+        val btnOn = findViewById<Button>(R.id.btnOn)
+        val btnOff = findViewById<Button>(R.id.btnOff)
+        val btnBack = findViewById<Button>(R.id.btnBack)
+
+        btnOn.setOnClickListener {
+            Toast.makeText(this, "Riego encendido", Toast.LENGTH_SHORT).show()
+        }
+
+        btnOff.setOnClickListener {
+            val rootView = findViewById<android.view.View>(R.id.btnOff)
+            Snackbar.make(rootView, "Riego apagado", Snackbar.LENGTH_SHORT).show()
+        }
+
+        btnBack.setOnClickListener {
+            finish() // vuelve a la actividad anterior (Dashboard)
+        }
+    }
+}
