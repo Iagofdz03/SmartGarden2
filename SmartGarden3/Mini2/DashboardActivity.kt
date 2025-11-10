3bb4d6c4d2f12ce924f7bb54b12e0f2e01417d66
Iagofdz03 <iagofzdz@gmail.com>
2025-11-10 14:19:22 +0100

    Create DashboardActivity.kt

---
+++ SmartGarden3/Mini2/DashboardActivity.kt
@@ -0,0 +1,25 @@
+package com.example.smartgraden3
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+class DashboardActivity : BaseMenuActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dashboard)
+        title = "Dashboard"
+
+        val btnRiego = findViewById<Button>(R.id.btnRiego)
+        val btnSensores = findViewById<Button>(R.id.btnSensores)
+
+        btnRiego.setOnClickListener {
+            startActivity(Intent(this, RiegoActivity::class.java))
+        }
+
+        btnSensores.setOnClickListener {
+            startActivity(Intent(this, SensoresActivity::class.java))
+        }
+    }
+}
